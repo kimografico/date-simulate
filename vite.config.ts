3595d3c5655ccdc92bfdd6b6e949d42553047fda
Carlos Saez <carsaebla@alu.edu.gva.es>
2026-07-31 19:51:52 +0200

    deploy to gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig(() => {
   return {
+    base: '/date-simulate/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
